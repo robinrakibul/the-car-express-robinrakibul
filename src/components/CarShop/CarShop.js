66ff0c669017e5d48443303684684ce7ce41fe1b
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-25 22:48:29 +0600

    added useState, function called, send data to product module

--- src/components/CarShop/CarShop.js
+++ src/components/CarShop/CarShop.js
@@ -1,20 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './CarShop.css';
 const CarShop = () => {
-    
+    const [products, setProduct] = useState([]);
 
     useEffect( () =>{
         fetch('car.json')
         .then(res=> res.json())
-        .then(data =>console.log(data))
+        .then(data =>setProduct(data))
     }, []);
 
     return (
         <div className='shop-container'>
             <div className="cards-container">
-                <Product></Product>
+            {
+                products.map(product=> <Product key={product.id} product={product}></Product>)
+            }
             </div>
             <div className="cart-container">
                 <Cart></Cart>
