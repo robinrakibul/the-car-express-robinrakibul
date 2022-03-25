fe2e38e4954b6f1588077d9167e93321ad9aef53
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-25 22:36:56 +0600

    useEffect console logging api data

--- src/components/CarShop/CarShop.js
+++ src/components/CarShop/CarShop.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './CarShop.css';
 const CarShop = () => {
+    
+
+    useEffect( () =>{
+        fetch('car.json')
+        .then(res=> res.json())
+        .then(data =>console.log(data))
+    }, []);
+
     return (
         <div className='shop-container'>
             <div className="cards-container">
