e95e3a5183254642315198373da321a1553ea2f9
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-25 21:55:29 +0600

    Heading style added, bootstrap added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,8 @@ import './Header.css';
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <h1 className='heading-text text-success mt-3 text-center'>The Car Express</h1>
+            <p className='sub-heading text-center'>Reasonable Cars for the Money</p>
         </div>
     );
 };
