f16c9ab2f4e643a5355699c4cb110cb32c293294
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-26 14:36:57 +0600

    project done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='mb-5'>
             <h1 className='heading-text text-success mt-3 text-center'>The Car Express</h1>
-            <p className='sub-heading text-center text-primary'>Reasonable Cars for the Money</p>
+            <p className='sub-heading text-center text-primary'>Buy your favourite cars in reasonable price</p>
         </div>
     );
 };
